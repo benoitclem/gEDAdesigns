1cfa74c275b3735bc4b3487947bc62df5b2fb038
Clément BENOIT <clemi@hotspot.lan>
2016-08-30 12:47:50 +0200

    add Full V2

---
+++ coaFullV2/coa.cmd
@@ -0,0 +1,252 @@
+# Pin name action command file
+
+# Start of element U4
+ChangePinName(U4, 1, GND)
+ChangePinName(U4, 2, OUTPUT)
+ChangePinName(U4, 3, INPUT)
+
+# Start of element CONN1
+ChangePinName(CONN1, 4, GND)
+ChangePinName(CONN1, 3, D+)
+ChangePinName(CONN1, 2, D-)
+ChangePinName(CONN1, 1, VCC)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element CONN5
+ChangePinName(CONN5, 1, 1)
+ChangePinName(CONN5, 2, 2)
+
+# Start of element C15
+ChangePinName(C15, 2, 2)
+ChangePinName(C15, 1, 1)
+
+# Start of element SAO1
+ChangePinName(SAO1, 1, 1)
+
+# Start of element C14
+ChangePinName(C14, 2, 2)
+ChangePinName(C14, 1, 1)
+
+# Start of element C13
+ChangePinName(C13, 2, 2)
+ChangePinName(C13, 1, 1)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element U7
+ChangePinName(U7, 16, VDD)
+ChangePinName(U7, 15, "res(VDD)")
+ChangePinName(U7, 14, "CAP(GND)")
+ChangePinName(U7, 13, GND)
+ChangePinName(U7, 12, GND)
+ChangePinName(U7, 11, "res(GND)")
+ChangePinName(U7, 10, "res(GND)")
+ChangePinName(U7, 9, "res(GND)")
+ChangePinName(U7, 8, DEN)
+ChangePinName(U7, 7, INT1)
+ChangePinName(U7, 6, DRDY/INT2)
+ChangePinName(U7, 5, CS)
+ChangePinName(U7, 4, SDO/SA0)
+ChangePinName(U7, 3, SDA/SDI/SDO)
+ChangePinName(U7, 2, SCL/SCP)
+ChangePinName(U7, 1, VDDIO)
+
+# Start of element U6
+ChangePinName(U6, 16, GND)
+ChangePinName(U6, 15, C1)
+ChangePinName(U6, 14, VDD)
+ChangePinName(U6, 13, GND)
+ChangePinName(U6, 12, GND)
+ChangePinName(U6, 11, INT1)
+ChangePinName(U6, 10, "res(GND)")
+ChangePinName(U6, 9, INT2)
+ChangePinName(U6, 8, CS)
+ChangePinName(U6, 7, SDO/SA0)
+ChangePinName(U6, 6, SDA/SDI/SDO)
+ChangePinName(U6, 5, GND)
+ChangePinName(U6, 4, SCL/SCP)
+ChangePinName(U6, 3, SETP)
+ChangePinName(U6, 2, SETC)
+ChangePinName(U6, 1, VDDIO)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element U5
+ChangePinName(U5, 18, res)
+ChangePinName(U5, 17, SCL)
+ChangePinName(U5, 16, SDA)
+ChangePinName(U5, 15, V_ANT/res)
+ChangePinName(U5, 14, VCC_RF)
+ChangePinName(U5, 13, ANT_ON/res)
+ChangePinName(U5, 12, GND)
+ChangePinName(U5, 11, RF_IN)
+ChangePinName(U5, 10, GND)
+ChangePinName(U5, 9, RESET_N)
+ChangePinName(U5, 8, VCC)
+ChangePinName(U5, 7, VCC_IO)
+ChangePinName(U5, 6, VBCKP)
+ChangePinName(U5, 5, EXTINT)
+ChangePinName(U5, 4, 1PPS)
+ChangePinName(U5, 3, RXD)
+ChangePinName(U5, 2, TXD)
+ChangePinName(U5, 1, GND)
+
+# Start of element D4
+ChangePinName(D4, 2, 2)
+ChangePinName(D4, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element D3
+ChangePinName(D3, 2, 2)
+ChangePinName(D3, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element F1
+ChangePinName(F1, 2, 2)
+ChangePinName(F1, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element SOCK1
+ChangePinName(SOCK1, 9, Shield)
+ChangePinName(SOCK1, 1, DATA2)
+ChangePinName(SOCK1, 2, CD/DAT3)
+ChangePinName(SOCK1, 3, CMD)
+ChangePinName(SOCK1, 4, VDD)
+ChangePinName(SOCK1, 6, VSS)
+ChangePinName(SOCK1, 5, CLK)
+ChangePinName(SOCK1, 7, DATA0)
+ChangePinName(SOCK1, 8, DATA1)
+
+# Start of element U3
+ChangePinName(U3, 5, VREF)
+ChangePinName(U3, 6, CANL)
+ChangePinName(U3, 7, CANH)
+ChangePinName(U3, 8, RS)
+ChangePinName(U3, 4, RXD)
+ChangePinName(U3, 3, VDD)
+ChangePinName(U3, 1, TXD)
+ChangePinName(U3, 2, VSS)
+
+# Start of element U2
+ChangePinName(U2, 5, VREF)
+ChangePinName(U2, 6, CANL)
+ChangePinName(U2, 7, CANH)
+ChangePinName(U2, 8, RS)
+ChangePinName(U2, 4, RXD)
+ChangePinName(U2, 3, VDD)
+ChangePinName(U2, 1, TXD)
+ChangePinName(U2, 2, VSS)
+
+# Start of element U1
+ChangePinName(U1, 28, DIP28)
+ChangePinName(U1, 25, DIP25)
+ChangePinName(U1, 26, DIP26)
+ChangePinName(U1, 24, DIP24)
+ChangePinName(U1, 27, DIP27)
+ChangePinName(U1, 23, DIP23)
+ChangePinName(U1, 21, DIP21)
+ChangePinName(U1, 22, DIP22)
+ChangePinName(U1, 36, RD-)
+ChangePinName(U1, 34, TD-)
+ChangePinName(U1, 35, RD+)
+ChangePinName(U1, 33, TD+)
+ChangePinName(U1, 32, D-)
+ChangePinName(U1, 31, D+)
+ChangePinName(U1, 29, DIP29)
+ChangePinName(U1, 30, DIP30)
+ChangePinName(U1, 39, VU_5V)
+ChangePinName(U1, 38, IF_D-)
+ChangePinName(U1, 37, IF_D+)
+ChangePinName(U1, 40, VR_33V)
+ChangePinName(U1, 13, DIP13)
+ChangePinName(U1, 16, DIP16)
+ChangePinName(U1, 15, DIP15)
+ChangePinName(U1, 17, DIP17)
+ChangePinName(U1, 14, DIP14)
+ChangePinName(U1, 18, DIP18)
+ChangePinName(U1, 20, DIP20)
+ChangePinName(U1, 19, DIP19)
+ChangePinName(U1, 5, DIP5)
+ChangePinName(U1, 7, DIP7)
+ChangePinName(U1, 6, DIP6)
+ChangePinName(U1, 8, DIP8)
+ChangePinName(U1, 9, DIP9)
+ChangePinName(U1, 10, DIP10)
+ChangePinName(U1, 12, DIP12)
+ChangePinName(U1, 11, DIP11)
+ChangePinName(U1, 2, VIN)
+ChangePinName(U1, 3, VB)
+ChangePinName(U1, 4, NR)
+ChangePinName(U1, 1, GND)
